5f8e873cd7500826b3fde94eb85d877aa0f22f08
0yassin <yassinz8275@gmail.com>
2026-04-26 20:18:57 +0100

    improved layout design

--- localifyapp/app/(tabs)/index.tsx
+++ localifyapp/app/(tabs)/index.tsx
@@ -9,9 +9,26 @@ export default function TabOneScreen() {
         <Text className='text-[31px] font-poppinsBold'>Localify</Text>
       </View>
 
-      <View>
+      <View className=''>
         <Text className='text-[25px] font-poppinsBold mb-4'>Synced playlists</Text>
-        <Card Title='Hello im card' Details='card thing' ImagePath='placeholder' />
+        <View className='gap-3 flex flex-col'>
+
+        <Card Title='peak songs' Details='20 tracks' ImagePath='placeholder' />
+        <Card Title='peak songs' Details='20 tracks' ImagePath='placeholder' />
+
+        <View className='w-full p-4 rounded-[6px] border-2 border-black/70 border-dashed flex-row items-center px-6 '>
+          <View className='flex flex-row justify-between items-center w-full'> 
+            <Text className='text-[18px] font-poppinsMedium text-textColor/70  '>Add playlist</Text>
+            <Text className='text-[21px] font-poppinsMedium  text-textColor/70'>+</Text>
+
+          </View>
+          <View>
+            <Text className='text-[18px] text-white font-poppinsSemiBold -m-1'></Text>
+            <Text className="text-[16px] text-white/70 font-poppinsMedium"></Text>
+          </View>
+          </View>
+
+        </View>
       </View>
     </View>
   );
--- localifyapp/components/Card.tsx
+++ localifyapp/components/Card.tsx
@@ -1,4 +1,5 @@
 import { View, Text } from "react-native"
+import { Shadow } from 'react-native-shadow-2';
 
 interface CardProps{
     Title: string,
@@ -8,14 +9,24 @@ interface CardProps{
 
 export default function Card({Title, ImagePath, Details}: CardProps){
     return(
-        <View className="flex flex-row px-3 py-3 items-center gap-4 border-2 border-black rounded-[6px]">
+    <View className="w-full">
+    <Shadow stretch 
+    
+      offset={[2, 2]} 
+      distance={0} 
+      startColor={'#000000'}
+      style={{ borderRadius: 6 }} 
+    >
+        <View className="flex bg-card flex-row px-2 py-2 items-center gap-3 border-2 border-black rounded-[6px]">
             <View className="bg-teal-800 p-10 aspect-square rounded-[4px]">
             </View>
             
             <View className="">
-                <Text className="text-[18px] -mb-1 font-poppinsSemiBold">Playlist Name</Text>
-                <Text className="text[16px] font-poppinsMedium">20 tracks</Text>
+                <Text className="text-[18px] -mb-1 font-poppinsSemiBold">{Title}</Text>
+                <Text className="text[16px] font-poppinsMedium">{Details}</Text>
             </View>
         </View>
+    </Shadow>
+    </View>
     )
 }
